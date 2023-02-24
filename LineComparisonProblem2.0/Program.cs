de973923c9d8afe9d4c67c47c1ddc2a31c489c87
Poojachakor <chakorpooja07@gmail.com>
2023-02-24 14:06:07 +0530

    Add: UC1 file

---
+++ LineComparisonProblem2.0/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LineComparisonProblem2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LengthOfLine.LengthOfLines();
+        }
+    }
+}
